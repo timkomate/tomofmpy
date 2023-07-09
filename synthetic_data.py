cd5eff2f80ea19c46f1ee774934024d5a9d9d1dc
Mate Timko <timko.mate@gmail.com>
2023-07-09 13:17:44 +0200

    Formatting
    Now the python scripts have better formatting

--- synthetic_data.py
+++ synthetic_data.py
@@ -5,8 +5,10 @@ import utils.tomo_eikonal as tomo_eikonal
 import PIL.Image
 import PIL.ImageOps
 
+
 def checkerboard(shape, tile_size):
     return (np.indices(shape) // tile_size).sum(axis=0) % 2
+
 
 def read_image(filepath, dv, v0):
     im = PIL.Image.open(filepath)
@@ -15,60 +17,72 @@ def read_image(filepath, dv, v0):
     velocity_model = np.flipud(velocity_model)
     return velocity_model
 
-def random_geometry(fname, xmin, xmax, ymin, ymax, sigma, nrec, nsrc, latlon = False, plot = False, seed = 1234):
+
+def random_geometry(
+    fname,
+    xmin,
+    xmax,
+    ymin,
+    ymax,
+    sigma,
+    nrec,
+    nsrc,
+    latlon=False,
+    plot=False,
+    seed=1234,
+):
     np.random.seed(seed)
 
     xr = xmin + (xmax - xmin) * np.random.rand(nrec)
     yr = ymin + (ymax - ymin) * np.random.rand(nrec)
     coord_rec = np.vstack([xr, yr]).T
 
-    xs = np.linspace(ymin,ymax,nsrc)
-    ys = np.linspace(xmin,xmax,nsrc)
-    a = np.tile(ymin,(nsrc))
-    b = np.tile(xmax,(nsrc))
-    c = np.tile(ymax,(nsrc))
-    d = np.tile(xmin,(nsrc))
-
+    xs = np.linspace(ymin, ymax, nsrc)
+    ys = np.linspace(xmin, xmax, nsrc)
+    a = np.tile(ymin, (nsrc))
+    b = np.tile(xmax, (nsrc))
+    c = np.tile(ymax, (nsrc))
+    d = np.tile(xmin, (nsrc))
 
     coords1 = np.vstack([ys, a])
     coords2 = np.vstack([b, xs])
     coords3 = np.vstack([ys, c])
     coords4 = np.vstack([d, xs])
-    coords_source = np.hstack([coords1,coords2, coords3, coords4]).T
-    coords_source = (np.unique(coords_source, axis=0))
+    coords_source = np.hstack([coords1, coords2, coords3, coords4]).T
+    coords_source = np.unique(coords_source, axis=0)
     n = coords_source.shape[0]
 
-    data = np.zeros((n*r,6))
+    data = np.zeros((n * r, 6))
     c = 0
-    for i,source in enumerate(coords_source):
-        for j,rec in enumerate(coord_rec):
-            data[c] = [i,source[0], source[1], rec[0], rec[1], sigma]
+    for i, source in enumerate(coords_source):
+        for j, rec in enumerate(coord_rec):
+            data[c] = [i, source[0], source[1], rec[0], rec[1], sigma]
             c += 1
     if latlon:
-        df = pd.DataFrame(data, columns = ["source_id", "lons", "lats", "lonr", "latr", "sigma"])
+        df = pd.DataFrame(
+            data, columns=["source_id", "lons", "lats", "lonr", "latr", "sigma"]
+        )
     else:
-        df = pd.DataFrame(data, columns = ["source_id", "xs", "ys", "xr", "yr", "sigma"])
-    df.astype({'source_id': 'int32'})
-    df.to_csv(
-        path_or_buf= fname,
-        index=False
-    )
+        df = pd.DataFrame(data, columns=["source_id", "xs", "ys", "xr", "yr", "sigma"])
+    df.astype({"source_id": "int32"})
+    df.to_csv(path_or_buf=fname, index=False)
     if plot:
         plt.scatter(coords1[0], coords1[1])
         plt.scatter(coords2[0], coords2[1])
         plt.scatter(coords3[0], coords3[1])
         plt.scatter(coords4[0], coords4[1])
-        plt.xlim([xmin,xmax])
-        plt.ylim([ymin,ymax])
+        plt.xlim([xmin, xmax])
+        plt.ylim([ymin, ymax])
         plt.show()
     return df
+
 
 if __name__ == "__main__":
     y = 120
     x = 120
-    xmin, xmax = 1,119
-    ymin, ymax = 1,119
-    r  = 500
+    xmin, xmax = 1, 119
+    ymin, ymax = 1, 119
+    r = 500
     nsrc = 10
     latlon = False
     plot = False
@@ -82,30 +96,34 @@ if __name__ == "__main__":
     x = 120
 
     fname = "synthetic_geometry_xy.csv"
-    df = random_geometry(fname, xmin, xmax, ymin,ymax,sigma, r, nsrc, latlon= False)
-    velocity_model = checkerboard((120,120), 20) * dv + v0
+    df = random_geometry(fname, xmin, xmax, ymin, ymax, sigma, r, nsrc, latlon=False)
+    velocity_model = checkerboard((120, 120), 20) * dv + v0
 
-    ny,nx = velocity_model.shape
-    dx, dy = x/nx,y/ny
+    ny, nx = velocity_model.shape
+    dx, dy = x / nx, y / ny
 
     # Solve Eikonal at source
-    eik = tomo_eikonal.Eikonal_Solver(velocity_model, gridsize=(dy, dx), filename="./synthetic_geometry_xy.csv", \
-                                BL = (11.5,44.4))
+    eik = tomo_eikonal.Eikonal_Solver(
+        velocity_model,
+        gridsize=(dy, dx),
+        filename="./synthetic_geometry_xy.csv",
+        BL=(11.5, 44.4),
+    )
     if latlon:
         eik.transform2xy()
 
     if plot:
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        image = ax.pcolor(eik.xaxis, eik.zaxis,eik.grid)
+        image = ax.pcolor(eik.xaxis, eik.zaxis, eik.grid)
         plt.colorbar(image)
         ax.set_aspect(1)
         plt.show()
 
-        plt.pcolor(eik.xaxis, eik.zaxis,eik.grid)
+        plt.pcolor(eik.xaxis, eik.zaxis, eik.grid)
         plt.colorbar()
-        plt.scatter(eik.df["xs"], eik.df["ys"], marker="h", c = "k")
-        plt.scatter(eik.df["xr"], eik.df["yr"], color = 'r', marker="^")
+        plt.scatter(eik.df["xs"], eik.df["ys"], marker="h", c="k")
+        plt.scatter(eik.df["xr"], eik.df["yr"], color="r", marker="^")
         plt.xlim([xmin, xmax])
         plt.ylim([ymin, ymax])
         plt.show()
--- utils/tomo_eikonal.py
+++ utils/tomo_eikonal.py
@@ -9,19 +9,20 @@ import scipy.optimize
 from scipy.spatial import ConvexHull
 import os
 
+
 class Eikonal_Solver(Eikonal2D):
-    def __init__(self, grid, gridsize, filename, origin = None, BL = (0,0)):
+    def __init__(self, grid, gridsize, filename, origin=None, BL=(0, 0)):
         super().__init__(grid, gridsize, origin)
         self.df = self._add_measurements(filename)
-        if("sigma" in self.df.columns):
+        if "sigma" in self.df.columns:
             sigma = 1 / self.df["sigma"].to_numpy()
         else:
             sigma = np.ones((len(self.df)))
         self.Cd = np.zeros((sigma.size, sigma.size))
-        np.fill_diagonal(self.Cd,sigma)
+        np.fill_diagonal(self.Cd, sigma)
         self.traveltimes = np.zeros(len(self.df))
         self.base = BL
-    
+
     def _init_transformer(self):
         lon_min = np.min([np.min(self.df["lons"]), np.min(self.df["lonr"])])
         lat_min = np.min([np.min(self.df["lats"]), np.min(self.df["latr"])])
@@ -29,42 +30,43 @@ class Eikonal_Solver(Eikonal2D):
         lat_max = np.max([np.max(self.df["lats"]), np.max(self.df["latr"])])
         lon = lon_min + (lon_max - lon_min) / 2
         lat = lat_min + (lat_max - lat_min) / 2
-        crs =  pyproj.CRS.from_proj4(f"+proj=laea +lat_0={lat} +lon_0={lon} +lat_b=0 +ellps=WGS84")
-        self.transformer = pyproj.Transformer.from_crs(4326,crs,always_xy=True)
-        self.inv_transformer = pyproj.Transformer.from_crs(crs,4326,always_xy=True)
+        crs = pyproj.CRS.from_proj4(
+            f"+proj=laea +lat_0={lat} +lon_0={lon} +lat_b=0 +ellps=WGS84"
+        )
+        self.transformer = pyproj.Transformer.from_crs(4326, crs, always_xy=True)
+        self.inv_transformer = pyproj.Transformer.from_crs(crs, 4326, always_xy=True)
 
     def transform2xy(self):
         self._init_transformer()
-        self.base = self.transformer.transform(self.base[0],self.base[1])
-        #self.TR = self.transformer.transform(self.TR[0],self.TR[1])
-        Es,Ns = self.transformer.transform(self.df["lons"],self.df["lats"])
-        Er,Nr = self.transformer.transform(self.df["lonr"],self.df["latr"])
+        self.base = self.transformer.transform(self.base[0], self.base[1])
+        # self.TR = self.transformer.transform(self.TR[0],self.TR[1])
+        Es, Ns = self.transformer.transform(self.df["lons"], self.df["lats"])
+        Er, Nr = self.transformer.transform(self.df["lonr"], self.df["latr"])
         self.df["xs"] = (Es - self.base[0]) / 1000
         self.df["ys"] = (Ns - self.base[1]) / 1000
         self.df["xr"] = (Er - self.base[0]) / 1000
         self.df["yr"] = (Nr - self.base[1]) / 1000
-    
-    def transform2latlon(self,X,Y):
-        #self._init_transformer()
-        lons, lats = self.inv_transformer.transform((X*1000+self.base[0]),(Y*1000+self.base[1]))
-        return lons,lats 
+
+    def transform2latlon(self, X, Y):
+        # self._init_transformer()
+        lons, lats = self.inv_transformer.transform(
+            (X * 1000 + self.base[0]), (Y * 1000 + self.base[1])
+        )
+        return lons, lats
 
     def update_model(self, x):
         self._grid = x
 
     def _add_measurements(self, filename):
-        df = pd.read_csv(
-            filepath_or_buffer = filename,
-            dtype={"source_id": np.uint32}
-        )
+        df = pd.read_csv(filepath_or_buffer=filename, dtype={"source_id": np.uint32})
         return df
 
     def _get_sources(self):
         sources = pd.unique(self.df["source_id"])
         s = np.zeros((sources.size, 2))
-        for i,source in enumerate(sources):
+        for i, source in enumerate(sources):
             subset = self.df[self.df["source_id"] == source]
-            s[i] = subset[["ys","xs"]].iloc[0].values
+            s[i] = subset[["ys", "xs"]].iloc[0].values
         return s
 
     def get_unique_stations(self, transform):
@@ -73,15 +75,17 @@ class Eikonal_Solver(Eikonal2D):
         else:
             stations = self.df[["lonr", "latr"]]
         return stations.to_numpy()
-    
+
     def _get_receivers(self, i):
         subset = self.df[self.df["source_id"] == i]
-        r = subset[["yr","xr"]].values
+        r = subset[["yr", "xr"]].values
         return r
-    
-    def solve(self, return_gradient = False, nsweep = 2):
+
+    def solve(self, return_gradient=False, nsweep=2):
         sources = self._get_sources()
-        self.tt = Eikonal2D.solve(self, sources = sources, nsweep = nsweep, return_gradient = return_gradient)
+        self.tt = Eikonal2D.solve(
+            self, sources=sources, nsweep=nsweep, return_gradient=return_gradient
+        )
         return self.tt
 
     def calc_traveltimes(self):
@@ -89,99 +93,116 @@ class Eikonal_Solver(Eikonal2D):
         for i, grid in enumerate(self.tt):
             points = self._get_receivers(i)
             n = points.shape[0]
-            self.traveltimes[offset:n + offset] = grid(points)
+            self.traveltimes[offset : n + offset] = grid(points)
             offset += n
-    
+
     def get_residuals(self):
         return self.df["tt"].to_numpy() - self.traveltimes
-    
+
     def calc_residuals(self):
         residuals = self.get_residuals()
         return (residuals @ self.Cd @ residuals) / len(self.df)
 
     def save_measurements(self, filename):
         self.df["tt"] = self.traveltimes
-        self.df.to_csv(
-            path_or_buf = "{}".format(filename),
-            index=False
-        )
-    
-    def add_noise(self, sigma, mu = 0):
+        self.df.to_csv(path_or_buf="{}".format(filename), index=False)
+
+    def add_noise(self, sigma, mu=0):
         noise = np.random.normal(mu, sigma, size=(self.traveltimes.size))
         self.traveltimes += noise
 
-class Eikonal_Inversion():
-    def __init__(self, modelspace, gridsize, filename, root_folder = "./", 
-        BL = (0,0), epsilon = 0, eta = 0, transform = False):
+
+class Eikonal_Inversion:
+    def __init__(
+        self,
+        modelspace,
+        gridsize,
+        filename,
+        root_folder="./",
+        BL=(0, 0),
+        epsilon=0,
+        eta=0,
+        transform=False,
+    ):
         self.y, self.x = modelspace
         self.ny, self.nx = gridsize
         self.transform = transform
-        
+
         self.n = self.nx * self.ny
-        self.dy,self.dx = self.y / gridsize[0], self.x / gridsize[1]
+        self.dy, self.dx = self.y / gridsize[0], self.x / gridsize[1]
         self.yaxis = np.arange(self.ny) * self.dy
         self.xaxis = np.arange(self.nx) * self.dx
         self.filename = filename
         self.root_folder = root_folder
-        if(not os.path.exists(self.root_folder)):
+        if not os.path.exists(self.root_folder):
             os.makedirs(root_folder)
         self.epsilon = epsilon
         self.eta = eta
-        
+
         self.residuals = []
-        self.eik = Eikonal_Solver(np.zeros((self.ny,self.nx)), gridsize=(self.dy, self.dx),filename=self.filename,BL=BL)
-        if (self.transform):
+        self.eik = Eikonal_Solver(
+            np.zeros((self.ny, self.nx)),
+            gridsize=(self.dy, self.dx),
+            filename=self.filename,
+            BL=BL,
+        )
+        if self.transform:
             self.eik.transform2xy()
         self.save_stations()
-        
+
     def fitness_func(self, x: np.array) -> np.float64:
-        velocity_model = x.reshape(self.ny,self.nx)
+        velocity_model = x.reshape(self.ny, self.nx)
         self.eik.update_model(velocity_model)
         self.eik.solve()
         self.eik.calc_traveltimes()
         fitness = self.eik.calc_residuals()
-        if (self.epsilon):
+        if self.epsilon:
             fitness += self.epsilon * self.calc_perturbation(x)
-        if (self.eta):
-            fitness += self.eta * self.calc_roughness(x) 
+        if self.eta:
+            fitness += self.eta * self.calc_roughness(x)
         return np.sqrt(fitness)
-    
+
     def fitness_func_PSO(self, x: np.array, dim) -> np.float64:
-        x = x.reshape(dim,self.ny*self.nx)
-        fittnesses = np.apply_along_axis(self.fitness_func,axis=1,arr = x)
+        x = x.reshape(dim, self.ny * self.nx)
+        fittnesses = np.apply_along_axis(self.fitness_func, axis=1, arr=x)
         return fittnesses
 
-    def calc_perturbation(self,x):
-        return (x-self.x0).T@(x-self.x0)
-    
-    def calc_roughness(self,x):
+    def fitness_func_PSO(self, x: np.array, dim) -> np.float64:
+        x = x.reshape(dim, self.ny * self.nx)
+        fittnesses = np.apply_along_axis(self.fitness_func, axis=1, arr=x)
+        return fittnesses
+
+    def calc_perturbation(self, x):
+        return (x - self.x0).T @ (x - self.x0)
+
+    def calc_roughness(self, x):
         D = self.calc_D()
         return x.T @ D @ x
 
     def calc_D(self):
-        D = np.zeros((self.n,self.n),dtype = np.float64)
+        D = np.zeros((self.n, self.n), dtype=np.float64)
         c = 0
         for i in np.arange(self.ny):
             for j in np.arange(self.nx):
-                D_tmp = np.zeros((self.ny,self.nx),dtype=np.float64)
+                D_tmp = np.zeros((self.ny, self.nx), dtype=np.float64)
                 cc = 0
-                if(i-1 >= 0):
+                if i - 1 >= 0:
                     cc += 1
-                    D_tmp[i-1,j] = -1
-                if(i+1 < self.ny):
+                    D_tmp[i - 1, j] = -1
+                if i + 1 < self.ny:
                     cc += 1
-                    D_tmp[i+1,j] = -1
-                if(j-1 >= 0):
+                    D_tmp[i + 1, j] = -1
+                if j - 1 >= 0:
                     cc += 1
-                    D_tmp[i,j-1] = -1
-                if(j+1 < self.nx):
+                    D_tmp[i, j - 1] = -1
+                if j + 1 < self.nx:
                     cc += 1
-                    D_tmp[i,j+1] = -1
-                D_tmp[i,j] = cc
-                D[c,:] = D_tmp.reshape(self.n)
+                    D_tmp[i, j + 1] = -1
+                D_tmp[i, j] = cc
+                D[c, :] = D_tmp.reshape(self.n)
                 c += 1
         return D
-    
+
     def callbackF(self, Xi):
         res = self.fitness_func(Xi)
         print(f"iteration: {self.Nfeval}, residual {res:.3f}")
@@ -190,9 +211,9 @@ class Eikonal_Inversion():
         self.save_model2(Xi, self.Nfeval)
         self.save_figure(Xi, self.Nfeval)
         self.Nfeval += 1
-    
-    def callbackF_EA(self, Xi,convergence):
-        #print(convergence)
+
+    def callbackF_EA(self, Xi, convergence):
+        # print(convergence)
         res = self.fitness_func(Xi)
         print(f"iteration: {self.Nfeval}, residual {res:.3f}")
         self.residuals.append(res)
@@ -200,15 +221,24 @@ class Eikonal_Inversion():
         self.save_model2(Xi, self.Nfeval)
         self.save_figure(Xi, self.Nfeval)
         self.Nfeval += 1
-    
-    def callbackF_DA(self, Xi,f, contex):
+
+    def callbackF_DA(self, Xi, f, contex):
         print(f"iteration: {self.Nfeval}, residual {f:.3f}")
         self.residuals.append(f)
         self.save_model(Xi, self.Nfeval)
         self.save_model2(Xi, self.Nfeval)
         self.save_figure(Xi, self.Nfeval)
         self.Nfeval += 1
-    
+
+    def callbackF_PSO(self, Xi):
+        res = self.fitness_func(Xi)
+        print(f"iteration: {self.Nfeval}, residual {res:.3f}")
+        self.residuals.append(f)
+        self.save_model(Xi, self.Nfeval)
+        self.save_model2(Xi, self.Nfeval)
+        self.save_figure(Xi, self.Nfeval)
+        self.Nfeval += 1
+
     def callbackF_PSO(self, Xi):
         res = self.fitness_func(Xi)
         print(f"iteration: {self.Nfeval}, residual {res:.3f}")
@@ -219,26 +249,28 @@ class Eikonal_Inversion():
         self.Nfeval += 1
 
     def save_model(self, Xi, iteration):
-        sol = Xi.reshape(self.ny,self.nx)
-        np.savetxt(f"{self.root_folder}/{iteration}.csv", sol, fmt = "%.2f", delimiter="," )
+        sol = Xi.reshape(self.ny, self.nx)
+        np.savetxt(
+            f"{self.root_folder}/{iteration}.csv", sol, fmt="%.2f", delimiter=","
+        )
 
     def save_model2(self, Xi, iteration):
         output = open(f"{self.root_folder}/{iteration}.xyz", "w")
         c = 0
         for i in self.yaxis:
             for j in self.xaxis:
-                if(self.transform):
-                    x, y = self.eik.transform2latlon(j,i)
+                if self.transform:
+                    x, y = self.eik.transform2latlon(j, i)
                 else:
-                    x,y = j,i
+                    x, y = j, i
                 output.write(f"{x:.3f} {y:.3f} {Xi[c]:.3f}\n")
                 c += 1
         output.close()
-    
+
     def save_figure(self, Xi, iteration):
-        sol = Xi.reshape(self.ny,self.nx)
+        sol = Xi.reshape(self.ny, self.nx)
         plt.figure()
-        plt.pcolor(self.xaxis, self.yaxis, sol) #vmax=1.9, vmin=2.1)
+        plt.pcolor(self.xaxis, self.yaxis, sol)  # vmax=1.9, vmin=2.1)
         plt.colorbar()
         plt.savefig(f"{self.root_folder}/{iteration}.png")
         plt.clf()
@@ -246,15 +278,14 @@ class Eikonal_Inversion():
 
     def save_residuals(self):
         res = np.array(self.residuals)
-        np.savetxt(f"{self.root_folder}/residuals.txt", X=res.T, fmt = "%.2f")
+        np.savetxt(f"{self.root_folder}/residuals.txt", X=res.T, fmt="%.2f")
 
     def save_stations(self):
         stations = self.eik.get_unique_stations(self.transform)
         hull = ConvexHull(stations)
         wrapper = stations[hull.vertices]
-        np.savetxt("{}/hull.xy".format(self.root_folder), wrapper, fmt = "%.2f")
-        np.savetxt("{}/stations.xy".format(self.root_folder), stations, fmt = "%.2f")
-
+        np.savetxt("{}/hull.xy".format(self.root_folder), wrapper, fmt="%.2f")
+        np.savetxt("{}/stations.xy".format(self.root_folder), stations, fmt="%.2f")
 
     def add_starting_model(self, x0):
         self.x0 = x0
@@ -262,21 +293,30 @@ class Eikonal_Inversion():
     def run_linearization(self, *args, **kwargs):
         self.Nfeval = 0
         self.callbackF(self.x0)
-        solver = scipy.optimize.minimize(self.fitness_func, self.x0, *args, **kwargs, callback=self.callbackF)
+        solver = scipy.optimize.minimize(
+            self.fitness_func, self.x0, *args, **kwargs, callback=self.callbackF
+        )
         self.callbackF(solver.x)
         self.save_residuals()
         return solver
 
-    def run_nonlinear(self, mode,*args, **kwargs):
+    def run_nonlinear(self, mode, *args, **kwargs):
         self.Nfeval = 0
-        if (mode == "GA"):
-            solver = scipy.optimize.differential_evolution(func = self.fitness_func, *args, **kwargs, callback=self.callbackF_EA)
-        elif (mode == "DA"):
-            solver = scipy.optimize.dual_annealing(func = self.fitness_func, *args, **kwargs, callback=self.callbackF_DA)
-        elif (mode == "PSO"):
+        if mode == "GA":
+            solver = scipy.optimize.differential_evolution(
+                func=self.fitness_func, *args, **kwargs, callback=self.callbackF_EA
+            )
+        elif mode == "DA":
+            solver = scipy.optimize.dual_annealing(
+                func=self.fitness_func, *args, **kwargs, callback=self.callbackF_DA
+            )
+        elif mode == "PSO":
             import pyswarms as ps
-            optimizer = ps.single.GlobalBestPSO(dimensions=self.ny*self.nx, *args, **kwargs)
-            solver = optimizer.optimize(self.fitness_func_PSO, iters=5, dim = 10)
+
+            optimizer = ps.single.GlobalBestPSO(
+                dimensions=self.ny * self.nx, *args, **kwargs
+            )
+            solver = optimizer.optimize(self.fitness_func_PSO, iters=5, dim=10)
         else:
             raise NotImplementedError
         return solver
